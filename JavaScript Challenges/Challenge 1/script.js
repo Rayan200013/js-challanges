68d52719c9beb91e8bf5dd2a8c597ab496e929a2
Rayan Bouezzeddine <113249440+Rayan200013@users.noreply.github.com>
2023-09-02 20:36:05 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -20,3 +20,24 @@ function fullName() {
   return `${fisrtName} ${lastName}`; //this is a function that return the full name of the user
 }
 console.error(fullName());
+
+
+
+let str = "this is a text";
+let num1 = 1215;
+let num2 = 999;
+let numTrue = true;
+let numFalse = false;
+let obj = {};
+let arr = [];
+let nullValue = null;
+let undefinedValue = undefined;
+console.log(str);
+console.log(num1 + typeof num1);
+console.log(typeof num2);
+console.log(typeof numTrue);
+console.log(typeof numFalse);
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof nullValue);
+console.log(typeof undefinedValue);
