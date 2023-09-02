cdbeb1bf9dd36b278020822ff0fb10749806942a
Rayan Bouezzeddine <113249440+Rayan200013@users.noreply.github.com>
2023-09-02 21:32:38 +0300

    Challenge 2 - Done with step 1

---
+++ JavaScript Challenges/Challenge 2/script.js
@@ -0,0 +1,22 @@
+// step one
+
+let age = 22;
+
+console.log(`my Age is: ${age}, can I enter the room?`);
+
+console.log(`${age} > 20?`);
+if (age >= 20) {
+  console.log("you can enter the room");
+} else {
+  console.log("you cant enter");
+}
+
+const expression1 = 5 > 3;
+const expression2 = 10 === "10";
+const expression3 = true && false;
+const expression4 = !expression3;
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
