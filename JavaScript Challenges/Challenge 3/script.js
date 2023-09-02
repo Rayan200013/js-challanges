f2e7b52b372fa969cd7bbb346034a444ecc328c4
Rayan Bouezzeddine <113249440+Rayan200013@users.noreply.github.com>
2023-09-03 00:02:19 +0300

    Challenge 3 - Done with step 1

---
+++ JavaScript Challenges/Challenge 3/script.js
@@ -0,0 +1,79 @@
+// ### Basic Function
+
+function addNumbers(num1, num2) {
+  return 1 + 2;
+}
+
+console.log(addNumbers());
+
+// ### Function with Conditional Statements
+
+function findMax(number1, number2) {
+  if (number1 > number2) {
+    return console.log("number one is greater:", number1);
+  } else if (number1 == number2) {
+    return console.log(
+      `the numbers are equal to each other: ${number1} = ${number2} `
+    );
+  } else {
+    return console.log("number two is the greatest:", number2);
+  }
+}
+
+console.log(findMax(2, 2));
+
+// ### Nested Function
+
+// first assumption
+// function compute(num1, num2, operation) {
+//   function arithmeticOperation(add, subtract, multiply, divide) {
+//     if (operation === "+") {
+//       console.log(add);
+//     } else if (operation === "-") {
+//       console.log(subtract);
+//     } else if (operation === "*") {
+//       return console.log(multiply);
+//     } else if (operation === "%") {
+//       return console.log(divide);
+//     } else {
+//       return "Invalid Operation";
+//     }
+//   }
+// }
+
+function compute(num1, num2, operation) {
+  function add() {
+    return num1 + num2;
+  }
+  function subtract() {
+    return num1 - num2;
+  }
+  function multiply() {
+    return num1 * num2;
+  }
+  function divide() {
+    if (num2 === 0) {
+      return "cannot divide by zero";
+    }
+    return num1 / num2;
+  }
+
+  switch (operation) {
+    case "+":
+      return add();
+    case "-":
+      return subtract();
+    case "*":
+      return multiply();
+    case "/":
+      return divide();
+    default:
+      "Invalid Operation";
+  }
+}
+
+console.log(`add:  ${compute(1, 2, "+")}`);
+console.log(`Substract: ${compute(10, 5, "-")}`);
+console.log(`Muliply: ${compute(10, 2, "*")}`);
+console.log(`Divide: ${compute(4, 2, "/")}`);
+console.log(`Divide ${compute(100, 0, "/")}`);
