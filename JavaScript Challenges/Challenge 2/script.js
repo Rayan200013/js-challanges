fe8e2efb0656cde37dff70337f568ad9ecfe4de5
Rayan Bouezzeddine <113249440+Rayan200013@users.noreply.github.com>
2023-09-02 21:53:04 +0300

    Challenge 2 - Done with step 2

--- JavaScript Challenges/Challenge 2/script.js
+++ JavaScript Challenges/Challenge 2/script.js
@@ -1,11 +1,11 @@
 // step one
 
-let age = 22;
+let age1 = 22;
 
-console.log(`my Age is: ${age}, can I enter the room?`);
+console.log(`my Age is: ${age1}, can I enter the room?`);
 
-console.log(`${age} > 20?`);
-if (age >= 20) {
+console.log(`${age1} > 20?`);
+if (age1 >= 20) {
   console.log("you can enter the room");
 } else {
   console.log("you cant enter");
@@ -20,3 +20,37 @@ console.log(expression1);
 console.log(expression2);
 console.log(expression3);
 console.log(expression4);
+
+// ## Step 2
+
+
+
+let age = 25;
+let hasDriverLicense = true;
+let hasCar = false;
+let canDrive = false;
+let canRentCar = false;
+let needDriver = false;
+console.log(`needDriver before ${needDriver}`);
+console.log(`canRentCar before ${canRentCar}`);
+console.log(canDrive);
+
+if (age >= 18 && hasDriverLicense == true) {
+  canDrive = true;
+} else {
+  needDriver = true;
+}
+console.log(`can Drive? ${canDrive}`);
+
+if (age >= 22 && hasDriverLicense == true && hasCar == false) {
+  canRentCar = true;
+} else {
+  canRentCar = false;
+}
+console.log(`Have a car? carRentCar after: ${canRentCar}`);
+
+if (age >= 18 && hasDriverLicense == false && hasCar == false) {
+  needDriver = true;
+} else {
+  needDriver = false;
+}
