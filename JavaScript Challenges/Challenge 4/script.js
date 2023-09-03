b3916aaaa76bd6ad99724cb4c75f12017e69c86c
Rayan Bouezzeddine <113249440+Rayan200013@users.noreply.github.com>
2023-09-03 11:19:54 +0300

    Challenge 4 - Done with step 1

---
+++ JavaScript Challenges/Challenge 4/script.js
@@ -0,0 +1,18 @@
+// ## Step 1: Arrays and Objects
+
+// ### Array Creation
+
+
+const fiveNumbers = [5, 2, 10, 7, 9];
+
+// ### Object Creation
+
+
+
+const personalInfo = {
+  name: "Rayan",
+  age: 22,
+  height: 185,
+  location: "Lebanon",
+  nationality: "Lebanese",
+};
