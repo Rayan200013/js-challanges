1f88327311509020650f9126083717ac8a9ea848
Rayan Bouezzeddine <113249440+Rayan200013@users.noreply.github.com>
2023-09-03 11:28:46 +0300

    Challenge 4 - Done with step 2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -2,12 +2,9 @@
 
 // ### Array Creation
 
-
 const fiveNumbers = [5, 2, 10, 7, 9];
 
 // ### Object Creation
-
-
 
 const personalInfo = {
   name: "Rayan",
@@ -16,3 +13,25 @@ const personalInfo = {
   location: "Lebanon",
   nationality: "Lebanese",
 };
+
+// ## Step 2: Spread Operator
+
+// ### New Array with Spread
+
+let nextThreeNumbers = [15, 20, 19];
+
+let allNumbers = { ...fiveNumbers, ...nextThreeNumbers };
+console.log(allNumbers);
+
+// ### New Object with Spread
+
+// Construct another object named `additionalInfo` with properties like occupation, hobby, and education.
+// Using the spread operator, create a new object named `fullInfo` which merges the properties from both `personalInfo` and `additionalInfo`.
+
+let additionalInfo = {
+  occupation: "bees",
+  hobby: "Singing",
+  education: "Management Information System",
+};
+let fullInfo = { ...personalInfo, ...additionalInfo };
+console.log(fullInfo);
